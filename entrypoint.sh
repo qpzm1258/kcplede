1a40238ca976206ec6162daeb2bc3796c6e8b03f
笑饮砒霜 <qpzm1258@163.com>
2018-03-02 12:42:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 kcpserver -t 127.0.0.1:8989 -l :29900 -key 131415 -crypt none -mode fast2 -mtu 1400 -sndwnd 2048 -rcvwnd 1024 -nocomp true 2>&1 &
 
-ss-server -s 0.0.0.0 -p 8989 -m aes-256-gcm -k 131415 
+ss-server -s 0.0.0.0 -p 8989 -m rc4-md5 -k 131415 
